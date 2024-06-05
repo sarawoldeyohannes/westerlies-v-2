5f9396ff23cae75f70d5da8a5c551e6754dd68ea
sarawoldeyohannes <sarawoldyohannes@gmail.com>
2024-06-05 07:08:44 +0300

    search psge

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Head } from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import FilterNavbar from "../../components/FilterNavbar/FilterNavbar";
+import MapComponent from "../../components/Map/MapComponent";
+import { searchitems } from "./controller.search";
+import Card from "../../components/Card/Card";
+import "./Search.css";
 
+const ITEMS_PER_PAGE = 5;
 const Search = () => {
+  const [isFooterVisible, setIsFooterVisible] = useState(false);
+  const footerRef = useRef<HTMLDivElement | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          setIsFooterVisible(entry.isIntersecting);
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (footerRef.current) {
+      observer.observe(footerRef.current);
+    }
+
+    return () => {
+      if (footerRef.current) {
+        observer.unobserve(footerRef.current);
+      }
+    };
+  }, [footerRef]);
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const selectedItems = searchitems.slice(
+    startIndex,
+    startIndex + ITEMS_PER_PAGE
+  );
+  const totalPages = Math.ceil(searchitems.length / ITEMS_PER_PAGE);
+
   return (
     <>
       <Head headerClassName="head-instance" />
-      <Footer />
+      <div className="search-container">
+        <FilterNavbar />
+        <div className="map-and-items">
+          <div className="items-pagination">
+            <div className="items-container">
+              {selectedItems.map((item) => (
+                <Card key={item.id} {...item} />
+              ))}
+            </div>
+            <div className="pagination">
+              {Array.from({ length: totalPages }, (_, index) => (
+                <button
+                  key={index + 1}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={currentPage === index + 1 ? "active" : ""}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={`map-container ${isFooterVisible ? "map-hidden" : ""}`}
+          >
+            <MapComponent />
+          </div>
+        </div>
+      </div>
+      <Footer ref={footerRef} type="Search" />
     </>
   );
 };
---
+++ src/pages/Search/controller.search.ts
@@ -0,0 +1,23 @@
+export const searchitems = [
+    { id: 1, title: 'Name1', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 2, title: 'Name2', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name3', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name4', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name5', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name6', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name7', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name8', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name9', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name10', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name11', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name12', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name13', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name14', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name15', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name16', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name17', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name18', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name19', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 3, title: 'Name20', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    // Add more items as needed
+  ];
