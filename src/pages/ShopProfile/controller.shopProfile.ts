f895b3125432f69cb7330a12eb72c7a8a1c840a9
sarawoldeyohannes <sarawoldyohannes@gmail.com>
2024-06-13 13:54:11 +0300

    edit

--- src/pages/ShopProfile/controller.shopProfile.ts
+++ src/pages/ShopProfile/controller.shopProfile.ts
@@ -1,8 +1,11 @@
 export const items = [
     { id: 1, title: 'Name', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
     { id: 2, title: 'Name', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
+    { id: 4, title: 'Name', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' },
     { id: 3, title: 'Name', description: 'Primary Tag', imageUrl: 'https://via.placeholder.com/300' }]
+    
     export const insta = [
         { id: 1,   imageUrl: 'https://via.placeholder.com/300' },
         { id: 2,   imageUrl: 'https://via.placeholder.com/300' },
+        { id: 4,   imageUrl: 'https://via.placeholder.com/300' },
         { id: 3,  imageUrl: 'https://via.placeholder.com/300' }]
