c0a9992c04d02b3ff1022b779323b8aabe33a538
sarawoldeyohannes <sarawoldyohannes@gmail.com>
2024-06-28 10:17:40 +0300

    headersearch

--- src/components/HeaderSearch/HeaderSearch.tsx
+++ src/components/HeaderSearch/HeaderSearch.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import FreeSearch from "../FreeSearch/FreeSearch";
+import "./HeaderSearch.css";
 import LocationSearchInput from "../LocationSearchInput/LocationSearchInput";
 import ProductSearchInput from "../ProductSearchInput/ProductSearchInput";
 
 const HeaderSearch = () => {
+  const [searchType, setSearchType] = useState("free");
+
   return (
-    <div>
-      <FreeSearch />
-      <LocationSearchInput />
-      <ProductSearchInput />
+    <div className="head-search">
+      <div className="buttons">
+        <div className="buttons-btn">
+          <button onClick={() => setSearchType("location")}>Location</button>
+        </div>
+        <div className="buttons-btn">
+          <button onClick={() => setSearchType("product")}>Product</button>
+        </div>
+      </div>
+      <div className="inputs">
+        {searchType === "free" && <FreeSearch />}
+        {searchType === "location" && <LocationSearchInput />}
+        {searchType === "product" && <ProductSearchInput />}
+      </div>
     </div>
   );
 };
